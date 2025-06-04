cc4aaf7b5f4a007b3d94ce2e8f5026277f00f7c8
ValerieTsan <valeriejean.tsan@gmail.com>
2025-06-04 13:33:37 -0400

    Added fonts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
 
 import "./App.css";
 import Main from "../Main/Main.jsx";
@@ -13,31 +11,17 @@ import Contact from "../Contact/Contact.jsx";
 import Footer from "../Footer/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+      <Main />
+      <AboutMe />
+      <Projects />
+      <Skills />
+      <Blog />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
