5fa5fae00cfa3bde227c88d15428a4e14edbe6ba
ValerieTsan <valeriejean.tsan@gmail.com>
2025-06-05 13:36:33 -0400

    Add heade and main css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,42 @@
 import "./Header.css";
 
 function Header() {
-  return <></>;
+  return (
+    <header className="header">
+      <div className="header__nav-bar">
+        <p className="header__logo">Valerie Tsan</p>
+        <nav className="header__desktop-nav">
+          <ul className="header__nav-list">
+            <li>
+              <a href="/" className="header__button-home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="header__button-saved">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/projects" className="header__button-projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="/skills" className="header__button-projects">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="header__button-projects">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import "./Main.css";
+import { FaGitHub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Main() {
-  return <></>;
+  return <main className="main"></main>;
 }
 
 export default Main;
