3033b54cb3a4c275c9a20159be6329088be28760
ValerieTsan <valeriejean.tsan@gmail.com>
2025-06-11 01:44:10 -0400

    Header hrefs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./Main.css";
-import { FaGitHub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+// import { FaGitHub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Main() {
-  return <main className="main"></main>;
+  return (
+    <section className="main">
+      <div className="main__content">
+        <div className="main__image"></div>
+        <h1 className="main__title">Valerie Tsan</h1>
+        <p className="main__subtitle">Software Engineer</p>
+        <p className="main__description">
+          I am a software engineer with a passion for building web applications.
+          I love creating user-friendly interfaces and solving complex problems.
+        </p>
+        <div className="main__hero-socials"></div>
+      </div>
+    </section>
+  );
 }
 
 export default Main;
