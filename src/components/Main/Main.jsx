32c1af462201113dd0ac6058390f87ee5d849d78
ValerieTsan <valeriejean.tsan@gmail.com>
2025-06-15 10:54:36 -0400

    Add Structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Main.css";
 import { FaGithub } from "react-icons/fa";
-// import { FaGitHub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGitHub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Main() {
   return (
     <section className="main">
       <div className="main__content">
         <div className="main__image"></div>
-        <h1 className="main__title">Valerie Tsan</h1>
+        <h1 className="main__title">Hello I'm Valerie</h1>
         <p className="main__subtitle">Software Engineer</p>
         <p className="main__description">
           I am a software engineer with a passion for building web applications.
