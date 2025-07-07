90a751d2334aac736785957c22f31ffde5fafbe9
ValerieTsan <valeriejean.tsan@gmail.com>
2025-07-06 21:00:17 -0400

    Add mobile media screen size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="header">
       <div className="header__nav-bar">
         <p className="header__logo">Valerie Tsan</p>
+
+        {/* Desktop Nav */}
         <nav className="header__desktop-nav">
           <ul className="header__nav-list">
             <li>
@@ -34,7 +44,48 @@ function Header() {
             </li>
           </ul>
         </nav>
+
+        {/* Mobile Menu Button */}
+        <button
+          className="header__menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+        </button>
       </div>
+
+      {/* Mobile Dropdown */}
+      {menuOpen && (
+        <nav className="header__mobile-nav">
+          <ul className="header__mobile-list">
+            <li>
+              <a href="/" onClick={handleLinkClick}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" onClick={handleLinkClick}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/projects" onClick={handleLinkClick}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="/skills" onClick={handleLinkClick}>
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="/contact" onClick={handleLinkClick}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
